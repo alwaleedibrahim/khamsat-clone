e4ee50ae0ef896ec434bc6804c3e90b79ae58002
alwaleedibrahim <alwaleedibramohammed@gmail.com>
2024-10-08 13:47:47 +0300

    FIX: fixed sidebar category filtering

--- src/app/[locale]/_components/filter-sidebar/Sidebar.tsx
+++ src/app/[locale]/_components/filter-sidebar/Sidebar.tsx
@@ -1,127 +1,191 @@
-'use client'
-import React, { useEffect } from 'react'
-import FilterCard from './FilterCard'
-import CardHeader from './CardHeader'
-import CardBody from './CardBody'
-import SearchInput from './form-control/SearchInput'
-import List from './List'
-import ListItem from './ListItem'
-import SubList from './SubList'
-import RatingInput from './form-control/RatingInput'
-import { FaTimes } from 'react-icons/fa'
-import CheckboxInput from './form-control/CheckBoxInput'
-import ICategory from '../../_models/category'
-import ISubCategory from '../../_models/subcategory'
-import { useDispatch, useSelector } from 'react-redux'
-import { getCategory, getSubCategory, selectCategory } from '../../_lib/redux/slice/categorySlice'
-import Link from 'next/link'
+"use client";
+import React, { useEffect } from "react";
+import FilterCard from "./FilterCard";
+import CardHeader from "./CardHeader";
+import CardBody from "./CardBody";
+import SearchInput from "./form-control/SearchInput";
+import List from "./List";
+import ListItem from "./ListItem";
+import SubList from "./SubList";
+import RatingInput from "./form-control/RatingInput";
+import { FaTimes } from "react-icons/fa";
+import CheckboxInput from "./form-control/CheckBoxInput";
+import ICategory from "../../_models/category";
+import ISubCategory from "../../_models/subcategory";
+import { TypedUseSelectorHook, useDispatch, useSelector   as useReduxSelector} from "react-redux";
+import {
+  getCategory,
+  getSubCategory,
+  selectCategory,
+  selectSubCategory,
+} from "../../_lib/redux/slice/categorySlice";
+import Link from "next/link";
+import { AppDispatch, RootState } from "../../_lib/redux/store";
 
-export default function Sidebar({filters: {category, subcategory}}: Readonly<{filters:{category: string, subcategory: string}}>) {
-  const dispatch = useDispatch()
-    useEffect(()=> {
-    dispatch(selectCategory(category))
-    dispatch(getCategory())
-    dispatch(getSubCategory(category))
-  },[])
-    const categories =  useSelector((state)=> state.category.categories)
-    const subcategories =  useSelector((state)=> state.category.subcategories)
-    const selectedCategory =  useSelector((state)=> state.category.selectedCategory)
+export default function Sidebar({
+  filters: { category, subcategory },
+}: Readonly<{ filters: { category: string; subcategory: string } }>) {
+  const dispatch = useDispatch<AppDispatch>();
+  useEffect(() => {
+    dispatch(selectCategory(decodeURIComponent(category)));
+    dispatch(selectSubCategory(decodeURIComponent(subcategory)));
+    dispatch(getCategory());
+    dispatch(getSubCategory(category));
+  },[category, dispatch, subcategory]);
+  const useSelector: TypedUseSelectorHook<RootState> = useReduxSelector;
+  const categories = useSelector((state) => state.category.categories);
+  const subcategories = useSelector((state) => state.category.subcategories);
+  const selectedCategory = useSelector(
+    (state) => state.category.selectedCategory
+  );
+  const selectedSubCategory = useSelector(
+    (state) => state.category.selectedSubCategory
+  );
   console.log(selectedCategory);
-  
-    
+  console.log(selectedSubCategory);
+
   return (
-    <div className='mx-20'>
-        <FilterCard>
-            <CardHeader><span>بحث</span></CardHeader>
-            <CardBody>
-                <SearchInput />
-            </CardBody>
-        </FilterCard>
-        <FilterCard>
-            <CardHeader><span>الاقسام</span></CardHeader>
-            <CardBody>
-                <List>
-                    {categories.map( (cat: ICategory)=> {
-                        return (<>
-                        {cat.name.en == selectedCategory? <>
-                        <Link href={`/ar/${cat.name.en}/subcat`} className='text-primary'><ListItem key={cat._id}>{cat.name.ar}</ListItem></Link>
-                        <ListItem>
-                            <SubList>
-                                {subcategories?.map((subcategory: ISubCategory)=> {
-                                    return (
-                                        <>
-                                    <ListItem key={subcategory._id}>{subcategory.title.ar}</ListItem>
-                                    <ListItem>
-                                        <SubList>
-                                        {subcategory.subcategories.map((nestedSubcategory: ISubCategory)=> {
+    <div className="mx-20">
+      <FilterCard>
+        <CardHeader>
+          <span>بحث</span>
+        </CardHeader>
+        <CardBody>
+          <SearchInput />
+        </CardBody>
+      </FilterCard>
+      <FilterCard>
+        <CardHeader>
+          <span>الاقسام</span>
+        </CardHeader>
+        <CardBody>
+          <List>
+            {categories.map((cat: ICategory) => {
+              return (
+                <>
+                  {cat.name.en == selectedCategory ? (
+                    <>
+                      <Link
+                        href={`/ar/${cat.name.en}/subcat`}
+                        className="text-primary hover:text-primary"
+                      >
+                        <ListItem key={cat._id}>{cat.name.ar}</ListItem>
+                      </Link>
+                      <ListItem>
+                        <SubList>
+                          {subcategories?.map((subcat: ISubCategory) => {
+                            return (
+                              <>
+                                <Link
+                                  href={`/ar/${cat.name.en}/${subcat.title.en}`}
+                                  className={`${
+                                    subcat.title.en == selectedSubCategory
+                                      ? `text-primary`
+                                      : ``
+                                  } hover:text-primary`}
+                                >
+                                  <ListItem key={subcat._id}>
+                                    {subcat.title.ar}
+                                  </ListItem>
+                                </Link>
+                                <ListItem>
+                                  <SubList>
+                                    {subcat.subcategories.map(
+                                      (nestedSubcategory: ISubCategory) => {
                                         return (
-                                        <ListItem key={nestedSubcategory._id}>{nestedSubcategory.title.ar}</ListItem>
-                                        )
-                                    }
-                                        )}
-                                        </SubList>
-                                    </ListItem>
-                                    </>
-                                )
-                                })}
-                            </SubList>
-                        </ListItem> </> 
-                        :  <Link href={`/ar/${cat.name.en}/subcat`}><ListItem key={cat._id}>{cat.name.ar}</ListItem></Link>
-                        }
-                        </>)
-                    })}
-                </List>
-            </CardBody>
-        </FilterCard>
-        <FilterCard>
-            <CardHeader>تقييم الخدمة <FaTimes /></CardHeader>
-            <CardBody>
-                <List>
-                    <ListItem>
-                        <RatingInput rating={4}/>
-                    </ListItem>
-                    <ListItem>
-                        <RatingInput rating={3}/>
-                    </ListItem>
-                    <ListItem>
-                        <RatingInput rating={2}/>
-                    </ListItem>
-                    <ListItem>
-                        <RatingInput rating={1}/>
-                    </ListItem>
-                </List>
-            </CardBody>
-        </FilterCard>
-        <FilterCard>
-            <CardHeader> مستوى البائع<FaTimes /></CardHeader>
-            <CardBody>
-                <List>
-                    <ListItem>
-                        <CheckboxInput name='level'>بائع موثوق / مميز</CheckboxInput>
-                    </ListItem>
-                    <ListItem>
-                        <CheckboxInput name='level'>بائع نشيط</CheckboxInput>
-                    </ListItem>
-                    <ListItem>
-                        <CheckboxInput name='level'>بائع جديد</CheckboxInput>
-                    </ListItem>
-                </List>
-            </CardBody>
-        </FilterCard>
-        <FilterCard>
-            <CardHeader> حالة البائع<FaTimes /></CardHeader>
-            <CardBody>
-                <List>
-                    <ListItem>
-                        <CheckboxInput name='status'>متواجد حاليا</CheckboxInput>
-                    </ListItem>
-                    <ListItem>
-                        <CheckboxInput name='status'>هوية موثقة</CheckboxInput>
-                    </ListItem>
-                </List>
-            </CardBody>
-        </FilterCard>
-        
+                                          <Link
+                                            key={nestedSubcategory._id}
+                                            href={`/ar/${cat.name.en}/${nestedSubcategory.title.en}`}
+                                            className={`${
+                                                nestedSubcategory.title.en == selectedSubCategory
+                                                ? `text-primary`
+                                                : ``
+                                            } hover:text-primary`}
+                                          >
+                                            <ListItem>
+                                              {nestedSubcategory.title.ar}
+                                            </ListItem>
+                                          </Link>
+                                        );
+                                      }
+                                    )}
+                                  </SubList>
+                                </ListItem>
+                              </>
+                            );
+                          })}
+                        </SubList>
+                      </ListItem>{" "}
+                    </>
+                  ) : (
+                    <Link href={`/ar/${cat.name.en}/subcat`} className="hover:text-primary">
+                      <ListItem key={cat._id}>{cat.name.ar}</ListItem>
+                    </Link>
+                  )}
+                </>
+              );
+            })}
+          </List>
+        </CardBody>
+      </FilterCard>
+      <FilterCard>
+        <CardHeader>
+          تقييم الخدمة <FaTimes />
+        </CardHeader>
+        <CardBody>
+          <List>
+            <ListItem>
+              <RatingInput rating={4} />
+            </ListItem>
+            <ListItem>
+              <RatingInput rating={3} />
+            </ListItem>
+            <ListItem>
+              <RatingInput rating={2} />
+            </ListItem>
+            <ListItem>
+              <RatingInput rating={1} />
+            </ListItem>
+          </List>
+        </CardBody>
+      </FilterCard>
+      <FilterCard>
+        <CardHeader>
+          {" "}
+          مستوى البائع
+          <FaTimes />
+        </CardHeader>
+        <CardBody>
+          <List>
+            <ListItem>
+              <CheckboxInput name="level">بائع موثوق / مميز</CheckboxInput>
+            </ListItem>
+            <ListItem>
+              <CheckboxInput name="level">بائع نشيط</CheckboxInput>
+            </ListItem>
+            <ListItem>
+              <CheckboxInput name="level">بائع جديد</CheckboxInput>
+            </ListItem>
+          </List>
+        </CardBody>
+      </FilterCard>
+      <FilterCard>
+        <CardHeader>
+          {" "}
+          حالة البائع
+          <FaTimes />
+        </CardHeader>
+        <CardBody>
+          <List>
+            <ListItem>
+              <CheckboxInput name="status">متواجد حاليا</CheckboxInput>
+            </ListItem>
+            <ListItem>
+              <CheckboxInput name="status">هوية موثقة</CheckboxInput>
+            </ListItem>
+          </List>
+        </CardBody>
+      </FilterCard>
     </div>
-  )
+  );
 }
--- src/app/[locale]/_lib/redux/slice/categorySlice.ts
+++ src/app/[locale]/_lib/redux/slice/categorySlice.ts
@@ -1,9 +1,4 @@
-import {
-  createAsyncThunk,
-  createSlice,
-  PayloadAction,
-  Slice,
-} from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, Slice } from "@reduxjs/toolkit";
 import ICategory from "@/app/[locale]/_models/category";
 import ISubCategory from "@/app/[locale]/_models/subcategory";
 import categoriesLoader from "../../axios/categoryLoader";
@@ -13,50 +8,72 @@ export interface ICategoryState {
   categories: ICategory[];
   subcategories: ISubCategory[];
   selectedCategory: string;
+  selectedSubCategory: string;
 }
 
 const initialState: ICategoryState = {
   categories: [],
   subcategories: [],
-  selectedCategory: ''
+  selectedCategory: "",
+  selectedSubCategory: "",
 };
 
-export const getCategory = createAsyncThunk("getCategories", async () => {
+export const getCategory = createAsyncThunk<
+  ICategory[],
+  void,
+  { rejectValue: string }
+>("getCategories", async (_, { rejectWithValue }) => {
   try {
     const categories: ICategory[] = await categoriesLoader();
     return categories;
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
   } catch (err) {
-    return err;
+    return rejectWithValue("Failed to load categories");
   }
 });
 
-export const getSubCategory = createAsyncThunk("getSubCategories", async (categoryName:string) => {
-    try {
-      const subcategories: ISubCategory[] = await subcategoriesLoader(categoryName);
-      return subcategories;
-    } catch (err) {
-      return err;
-    }
-  });
+export const getSubCategory = createAsyncThunk<
+  ISubCategory[],
+  string,
+  { rejectValue: string }
+>("getSubCategories", async (categoryName: string, { rejectWithValue }) => {
+  try {
+    const subcategories: ISubCategory[] = await subcategoriesLoader(
+      categoryName
+    );
+    return subcategories;
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (err) {
+    return rejectWithValue("Failed to load categories");
+  }
+});
 
 export const categorySlice: Slice = createSlice({
   name: "categorySlice",
   initialState,
   reducers: {
-    selectCategory: (state, action) => {        
+    selectCategory: (state, action) => {
       state.selectedCategory = action.payload;
+    },
+    selectSubCategory: (state, action) => {
+      state.selectedSubCategory = action.payload;
     },
   },
   extraReducers: (builder) => {
     builder.addCase(getCategory.fulfilled, (state, action) => {
       state.categories = action.payload;
     });
+    builder.addCase(getCategory.rejected, (state, action) => {
+      console.error(action.payload);
+    });
     builder.addCase(getSubCategory.fulfilled, (state, action) => {
       state.subcategories = action.payload;
+    });
+    builder.addCase(getSubCategory.rejected, (state, action) => {
+      console.error(action.payload);
     });
   },
 });
 
-
-export const { selectCategory} = categorySlice.actions;
+export const { selectCategory, selectSubCategory } = categorySlice.actions;
 export default categorySlice.reducer;
